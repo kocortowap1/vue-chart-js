d87f9a2af67e3be8de820bb79e9a681291935888
kocortowap1 <kocortowap1@gmail.com>
2020-03-09 15:32:27 +0700

    try barChart and lineChart

---
+++ src/components/BarChart.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="bar-chart">
+    <canvas id="chart" height="400" width="400"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js'
+export default {
+  name: "BarChart",
+  props: {
+    data: {
+      required: true,
+      type: Object,
+      default: () => {}
+    }, 
+    option: {
+        required: false,
+        type: Object,
+        default: () => {}
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    createChart(elID, chartData, chartOption) {
+      const ctx = document.getElementById(elID);
+      new Chart(ctx, {
+        type: "bar",
+        data: chartData,
+        options: chartOption
+      });
+    }
+  },
+  mounted(){
+      this.createChart('chart', this.data, this.option)
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="line-chart">
+    <canvas id="line" height="400" width="400"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js'
+export default {
+  name: "LineChart",
+  props: {
+    data: {
+      required: true,
+      type: Object,
+      default: () => {}
+    },
+    option: {
+      required: false,
+      type: Object,
+      default: () => {}
+    }
+  },
+  methods: {
+    createChart(elID, chartData, chartOption) {
+      const ctx = document.getElementById(elID);
+      new Chart(ctx, {
+        type: "line",
+        data: chartData,
+        options: chartOption
+      });
+    }
+  },
+  mounted() {
+    this.createChart("line", this.data, this.option);
+  }
+};
+</script>
+
+<style>
+</style>
