b8f4c23ef2e0a369d65ebf12235433c1237168b6
kocortowap1 <kocortowap1@gmail.com>
2020-03-09 15:31:34 +0700

    try barChart and lineChart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BarChart :data="chartData" :option="option" />
+    <LineChart :data="chartData" :option="option" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import BarChart from "../components/BarChart";
+// import LineChart from "../components/LineChart";
+import data from "../sample/bulanan.json";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    BarChart: () => import("../components/BarChart"),
+    LineChart : () => import("../components/LineChart"),
+  },
+  data() {
+    return {
+      data: data,
+      option: {
+        responsive: false,
+        title: {
+          display: true,
+          text: "Statistik Pendaftar Perbulan"
+        }
+      }
+    };
+  },
+  computed: {
+    chartData() {
+      const label = this.data.map(d => {
+        const formatBln = new Intl.DateTimeFormat("id-ID", { month: "long" });
+        const bln = new Date(Date.UTC(2020, Number(d.id_bulan) - 1, 1, 0, 0));
+        return formatBln.format(bln);
+      });
+      // const total = this.data.map(t => Number(t.total));
+      const totalL = this.data.map(j => Number(j.L));
+      const totalP = this.data.map(j => Number(j.P));
+
+      return {
+        labels: label,
+        datasets: [
+          {
+            label: "Laki-laki",
+            borderColor: "blue",
+            backgroundColor: "lightblue",
+            data: totalL,
+            fill: true
+          },
+          {
+            label: "Perempuan",
+            borderColor: "red",
+            backgroundColor: "pink",
+            data: totalP,
+            fill: true
+          }
+        ]
+      };
+    }
   }
-}
+};
 </script>
